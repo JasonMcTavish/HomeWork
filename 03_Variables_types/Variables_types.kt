81e71de1a8492535e62a452869cc0eda9da2c07d
Aleksandr Aleinikov <JasonMcTavish.work@gmail.com>
2022-01-10 14:24:21 +0300

    Домашнее задание 3

---
+++ 03_Variables_types/Variables_types.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val firstName: String = "Aleksandr"
+    val lastName: String = "Aleynikov"
+    var height: Double = 1.70
+    var weight: Float = 92.3f
+    var isChild = height < 1.5 || weight < 40f
+    var info = "My name is $firstName and my surname $lastName. " +
+            "My height is $height and weight is $weight. I'm a child is $isChild. "
+    println(info)
+    height = 1.45
+    isChild = height < 1.5 || weight < 40f
+
+    info = "My name is $firstName and my surname $lastName. " +
+            "My height is $height and weight is $weight. I'm a child is $isChild. "
+    println(info)
+
+}
