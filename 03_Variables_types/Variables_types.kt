e4533dbbedfafb0823ceddd3fd1543156358f58f
Aleksandr Aleinikov <JasonMcTavish.work@gmail.com>
2022-01-12 14:28:02 +0300

    Homework Functions.

--- 03_Variables_types/Variables_types.kt
+++ 03_Variables_types/Variables_types.kt
@@ -1,17 +1,20 @@
 fun main(){
-    val firstName: String = "Aleksandr"
-    val lastName: String = "Aleynikov"
-    var height: Double = 1.70
-    val weight: Float = 92.3f
-    var isChild = height < 1.5 || weight < 40f
-    var info = "My name is $firstName and my surname $lastName. " +
-            "My height is $height and weight is $weight. I'm a child is $isChild. "
-    println(info)
-    height = 1.45
-    isChild = height < 1.5 || weight < 40f
+//    val firstName: String = "Aleksandr"
+//    val lastName: String = "Aleynikov"
+//    var height: Double = 1.70
+//    val weight: Float = 92.3f
+//    val isChild = {a : Double, b: Float -> a < 1.5 || b < 40f}
+//    val info = {a: String,b: String,c: Double,d: Float,e: Boolean -> println("My name is $a and my surname $b. " +
+//            "My height is $c and weight is $d. I'm a child is $e. ")}
+//    info(firstName, lastName, height, weight, isChild(height,weight))
+//    height = 1.45
+//    info(firstName, lastName, height, weight, isChild(height,weight))
+    val codedString = """F2p)v"y233{0->c}ttelciFc"""
+    println(codedString.length)
+    var firstPartCodedString = codedString.dropLast(codedString.length/2)
+    var secondPartCodedString = codedString.drop(codedString.length/2)
 
-    info = "My name is $firstName and my surname $lastName. " +
-            "My height is $height and weight is $weight. I'm a child is $isChild. "
-    println(info)
-
+}
+fun firstPart (sourceString:String) {
+    val shiftedString = sourceString.map {char -> char - 1}.joinToString(“”)
 }
