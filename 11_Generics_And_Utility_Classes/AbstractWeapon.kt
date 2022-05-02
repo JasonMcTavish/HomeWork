e03c8b4b92aa67002c5f735326a54947e18c552b
JasonMcTavish <JasonMcTavish.work@gmail.com>
2022-05-02 19:27:42 +0300

    Часть 2 пункты 1-5

--- 11_Generics_And_Utility_Classes/AbstractWeapon.kt
+++ 11_Generics_And_Utility_Classes/AbstractWeapon.kt
@@ -2,20 +2,34 @@ package `11_Generics_And_Utility_Classes`
 
 class AbstractWeapon(val maxAmmo: Int) {
     var fireType = Any()
+    var typeOfAmmo = Ammo.PistolAmmo
+    var ammoMagazine = Stack<Ammo>()
 
-    val ammoMagazine = Stack<Ammo>()
+    private fun createAmmo(): Ammo =
+        when (typeOfAmmo) {
+            Ammo.PistolAmmo -> Ammo.PistolAmmo
+            Ammo.RifleAmmo -> Ammo.RifleAmmo
+            Ammo.MachineGunAmmo -> Ammo.MachineGunAmmo
+            Ammo.Grenade -> Ammo.Grenade
+        }
 
-    fun createAmmo(weapon: AbstractWeapon): Ammo {
-        when (weapon){
-
+    fun recharge() {
+        var numberOfAmmo: Int = 0
+        do {
+            ammoMagazine.push(createAmmo())
+            numberOfAmmo++
+        } while (numberOfAmmo == maxAmmo)
     }
-}
 
-fun recharge() {
-
-}
-
-fun getAmmo() {
-
-}
+    fun getAmmo() {
+        var i = 0
+        if (ammoMagazine.isEmpty()) recharge()
+        else {
+            if (fireType == FireType.SingleShot) ammoMagazine.pop()
+            else while (i != 4) {
+                ammoMagazine.pop()
+                i++
+            }
+        }
+    }
 }
--- 11_Generics_And_Utility_Classes/Weapons.kt
+++ 11_Generics_And_Utility_Classes/Weapons.kt
@@ -1,30 +1,33 @@
 package `11_Generics_And_Utility_Classes`
 
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 object Weapons {
     fun createPistol(): AbstractWeapon {
         val weapon = AbstractWeapon(Random.nextInt(0, 15))
         weapon.fireType = FireType.SingleShot
+        weapon.typeOfAmmo = Ammo.PistolAmmo
         return weapon
     }
 
     fun createRifle(): AbstractWeapon {
         val weapon = AbstractWeapon(Random.nextInt(0, 5))
         weapon.fireType = FireType.SingleShot
+        weapon.typeOfAmmo = Ammo.RifleAmmo
         return weapon
     }
 
     fun createMachineGun(): AbstractWeapon {
         val weapon = AbstractWeapon(Random.nextInt(0, 15))
         weapon.fireType = FireType.BurstShooting(5)
+        weapon.typeOfAmmo = Ammo.MachineGunAmmo
         return weapon
     }
 
     fun createGrenade(): AbstractWeapon {
         val weapon = AbstractWeapon(1)
         weapon.fireType = FireType.SingleShot
+        weapon.typeOfAmmo = Ammo.Grenade
         return weapon
     }
 }
