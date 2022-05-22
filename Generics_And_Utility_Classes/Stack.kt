d8b879f1e0978642c2f1446066243ddead130405
Aleksandr Aleinikov <JasonMcTavish.work@gmail.com>
2022-05-22 12:12:37 +0300

    На отладке

--- Generics_And_Utility_Classes/Stack.kt
+++ Generics_And_Utility_Classes/Stack.kt
@@ -1,20 +1,5 @@
 package Generics_And_Utility_Classes
 
 import java.util.Stack
-import java.util.ArrayDeque
 
-class Stack<T> {
-     private var collection= ArrayDeque<T>()
-
-    fun push(item: T) {
-        collection.push(item)
-    }
-
-    fun pop(): T {
-        return collection.pop()
-    }
-
-    fun isEmpty(): Boolean {
-        return collection.isEmpty()
-    }
-}
+class Stack<T>: Stack<T>()
