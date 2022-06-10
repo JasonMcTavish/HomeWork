09da69485708acce4559f9676a41d0840112e913
JasonMcTavish <JasonMcTavish.work@gmail.com>
2022-06-10 22:38:18 +0300

    Сделал через try catch.

--- Coroutines/Main.kt
+++ Coroutines/Main.kt
@@ -2,40 +2,66 @@ package Coroutines
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.GlobalScope
+import java.math.BigInteger
+import kotlin.coroutines.CoroutineContext
 
+val handler = CoroutineExceptionHandler { _, exception ->
+    println("Error: ${exception.message}")
+}
+val scope = CoroutineScope(Dispatchers.Default + handler)
 fun main(): Unit = runBlocking {
-    val handler = CoroutineExceptionHandler { _, exception ->
-        println("Time out - ${exception.message}")
-    }
     val fibo1 = Fibonacci
     val fibo2 = Fibonacci
     val fibo3 = Fibonacci
-    val n1 = 10000
+    val n1 = 1000
     val n2 = 80
-    val n3 = 3000
-    val job = GlobalScope.launch(handler) {
-        launch {
-            withTimeout(3600) {
-                val a = fibo1.take(n1)
+    val n3 = 300
+    var a: BigInteger
+    var b: BigInteger
+    var c: BigInteger
+
+    val job = scope.launch {
+        val job2 = launch {
+            for (i in 1..5000) {
+                delay(10)
+                if (i % 50 == 0) println(".")
+                else print(".")
+            }
+        }
+        try {
+            withTimeout(3000) {
+                a = fibo1.take(n1)
                 println("Fibo №$n1 is $a")
             }
+        } catch (e: TimeoutCancellationException) {
+            println("Time out")
         }
-        launch {
+
+        try {
             withTimeout(3000) {
-                val b = fibo2.take(n2)
+                b = fibo2.take(n2)
                 println("Fibo №$n2 is $b")
             }
+        } catch (e: TimeoutCancellationException) {
+            println("Time out")
         }
-        launch {
+        try {
             withTimeout(3000) {
-                val c = fibo3.take(n3)
+                c = fibo3.take(n3)
                 println("Fibo №$n3 is $c")
-
             }
+        } catch (e: TimeoutCancellationException) {
+            println("Time out")
         }
+
+
+        if (isActive) job2.cancelAndJoin()
+
     }
     job.join()
 }
+
+
 
 
 
